7def6ed7743c62a227164d9965279c58877b71f9
xianpeng sun <Xianpeng.Sun@lincolnuni.ac.nz>
2024-03-10 23:36:59 +1300

    commit 2

--- app.py
+++ app.py
@@ -20,15 +20,6 @@ app.secret_key = 'your secret key'
 dbconn = None
 connection = None
 
-def getCursor():
-    global dbconn
-    global connection
-    connection = mysql.connector.connect(user=connect.dbuser, \
-    password=connect.dbpass, host=connect.dbhost, auth_plugin='mysql_native_password',\
-    database=connect.dbname, autocommit=True)
-    dbconn = connection.cursor()
-    return dbconn
-
 def getDictCursor():
     global dbconn
     global connection
@@ -48,19 +39,19 @@ def login():
         username = request.form['username']
         user_password = request.form['password']
         # Check if account exists using MySQL
-        cursor = getCursor()
-        cursor.execute('SELECT * FROM secureaccount WHERE username = %s', (username,))
+        cursor = getDictCursor()
+        cursor.execute('SELECT * FROM user WHERE name = %s', (username,))
         # Fetch one record and return result
         account = cursor.fetchone()
         if account is not None:
-            password = account[2]
+            password = account["password"]
             if hashing.check_value(password, user_password, salt='abcd'):
             # If account exists in accounts table 
             # Create session data, we can access this data in other routes
                 session['loggedin'] = True
-                session['id'] = account[0]
-                session['username'] = account[1]
-                session['userrole'] = account[4]
+                session['id'] = account['user_id']
+                session['username'] = account['name']
+                session['userrole'] = account['role']
                 # Redirect to home page
                 return redirect(url_for('home'))
             else:
@@ -76,6 +67,7 @@ def login():
 def root():
     return redirect(url_for('home'))
 
+
 # http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -88,8 +80,8 @@ def register():
         password = request.form['password']
         email = request.form['email']
         # Check if account exists using MySQL
-        cursor = getCursor()
-        cursor.execute('SELECT * FROM secureaccount WHERE username = %s', (username,))
+        cursor = getDictCursor()
+        cursor.execute('SELECT * FROM user WHERE name = %s', (username,))
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
@@ -103,8 +95,7 @@ def register():
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
             hashed = hashing.hash_value(password, salt='abcd')
-            cursor.execute('INSERT INTO secureaccount VALUES (NULL, %s, %s, %s, %s)', (username, hashed, email, 1))
-            connection.commit()
+            cursor.execute('INSERT INTO user (name, password, email, role) VALUES (%s, %s, %s, %s)', (username, hashed, email, 1))
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
@@ -129,8 +120,8 @@ def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
-        cursor = getCursor()
-        cursor.execute('SELECT * FROM secureaccount WHERE id = %s', (session['id'],))
+        cursor = getDictCursor()
+        cursor.execute('SELECT * FROM user WHERE user_id = %s', (session['id'],))
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
@@ -149,14 +140,30 @@ def logout():
    return redirect(url_for('login'))
 
 
+@app.route('/staff_management')
+def staff_management():
+    if 'loggedin' in session:
+        cursor = getDictCursor()
+        cursor.execute('SELECT * FROM user WHERE user_id = %s', (session['id'],))
+        account = cursor.fetchone()
+        if account["role"] == 3:
+            cursor.execute('SELECT * FROM user WHERE role = 2')
+            users = cursor.fetchall()
+            return render_template('user_management.html', userrole=session['userrole'], users=users)
+        else:
+            return redirect(url_for('home'))
+    return redirect(url_for('login'))
+
 @app.route('/user_management')
 def user_management():
     if 'loggedin' in session:
-        cursor = getCursor()
-        cursor.execute('SELECT * FROM secureaccount WHERE id = %s', (session['id'],))
+        cursor = getDictCursor()
+        cursor.execute('SELECT * FROM user WHERE user_id = %s', (session['id'],))
         account = cursor.fetchone()
-        if account[4] == 2 or account[4] == 3:
-            return render_template('user_management.html', userrole=session['userrole'])
+        if account["role"] == 2 or account["role"] == 3:
+            cursor.execute('SELECT * FROM user WHERE role = 1')
+            users = cursor.fetchall()
+            return render_template('user_management.html', userrole=session['userrole'], users=users)
         else:
             return redirect(url_for('home'))
     return redirect(url_for('login'))
@@ -165,7 +172,7 @@ def user_management():
 def guide_management():
     if 'loggedin' in session:
         cursor = getDictCursor()
-        cursor.execute('SELECT * FROM secureaccount WHERE id = %s', (session['id'],))
+        cursor.execute('SELECT * FROM user WHERE user_id = %s', (session['id'],))
         account = cursor.fetchone()
         if session.get('userrole') in [2, 3]:
             cursor.execute('SELECT * FROM guide')
@@ -174,6 +181,50 @@ def guide_management():
         else:
             return redirect(url_for('home'))
     return redirect(url_for('login'))
+
+@app.route('/guides/delete/<int:guide_id>', methods=['POST'])
+def delete_guide(guide_id):
+    cursor = getDictCursor()
+    # Delete the guide from the database
+    sql = "DELETE FROM guide WHERE horticulture_id=%s"
+    cursor.execute(sql, (guide_id,))
+
+    return "Guide deleted successfully"
+
+@app.route('/guides/edit/<int:horticulture_id>', methods=['POST'])
+def edit_guide(horticulture_id):
+    data = request.json
+
+    cursor = getDictCursor()
+    sql = "UPDATE guide SET common_name=%s, scientific_name=%s, key_characteristics=%s, biology_description=%s, symptoms=%s WHERE horticulture_id=%s"
+    cursor.execute(sql, (
+        data['common_name'], data['scientific_name'], data['key_characteristics'], data['biology_description'],
+        data['symptoms'], horticulture_id))
+    return redirect(url_for('guide_management'))
+
+
+
+# Route for deleting a user
+@app.route('/delete_user/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    cursor = getDictCursor()
+    cursor.execute("DELETE FROM user WHERE user_id=%s", (user_id,))
+    return redirect(url_for('user_management'))
+
+# Route for updating a user
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    if request.method == 'POST':
+        user_id = request.form['id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+        cursor = getDictCursor()
+        sql = "UPDATE user SET first_name='%s', last_name='%s', phone_number='%s' WHERE user_id=%s" % (first_name, last_name, phone_number, user_id)
+        cursor.execute(sql)
+        return 'User information updated successfully!', 200  # Return success response
+    return 'Error updating user information', 500  # Return error response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
